d2575314bb44cd87cc900b4739af6b91d621f4ec
Fernando Silva <fsak0517@gmail.com>
2022-08-03 20:56:13 -0300

    Font family added, some changes in style

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -2,7 +2,31 @@ import './styles.css'
 export function Footer(){
     return (
         <div className="footer">
-
+            <ul>
+                <li>Meta</li>
+                <li>Sobre</li>
+                <li>Blog</li>
+                <li>Carreiras</li>
+                <li>Ajuda</li>
+                <li>API</li>
+                <li>Privacidade</li>
+                <li>Termos</li>
+                <li>Principais contas</li>
+                <li>Hashtags</li>
+                <li>Localizações</li>
+                <li>Instagram Lite</li>
+                <li>Carregamento de contatos e não usuarios</li>
+            </ul>
+            <ul>
+            <li>Dança</li>
+            <li>Comida e bebida</li>
+            <li>Casa e jardim</li>
+            <li>Música</li>
+            <li>Artes visuais</li>
+            </ul>
+            <address>
+                <span>all rights reserved to the Meta</span>
+            </address>
         </div>
     )
 }
