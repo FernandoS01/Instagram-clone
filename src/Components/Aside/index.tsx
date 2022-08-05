5a682cec6636e07a00d3e8d5a86cc6641dc7ae2f
Fernando Silva <fsak0517@gmail.com>
2022-08-05 19:17:32 -0300

    Changes in aside and styles/home

--- src/Components/Aside/index.tsx
+++ src/Components/Aside/index.tsx
@@ -1,15 +1,22 @@
 import './styles.css'
-import { useState } from 'react'
 import 'react-dom'
-export function Aside(){
-    const interval:number = 3000
-    const colors:string[] = ['Red','Green','Blue']
-    const content = document.querySelector('.content')
+import CellPhone from '../../Assets/cellphone1.png'
+import CellPhone2 from '../../Assets/cellphone2.png'
+import CellPhone3 from '../../Assets/cellphone3.png'
+import CellPhone4 from '../../Assets/cellphone4.png'
+
+
+export function Aside(){   
 
     return (
         <>
         <div className="aside">
-            <div className='content'></div>  
+            <div className='content'>
+                <img className='active'src={CellPhone} alt="" />
+                <img className='active'src={CellPhone2} alt="" />
+                <img className='active'src={CellPhone3} alt="" />
+                <img className='active'src={CellPhone4} alt="" />    
+            </div>  
         </div>
         </>
     )
